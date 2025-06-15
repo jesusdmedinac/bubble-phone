d58618cdca48168f40730d929f503e8c1dbf65b0
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2025-06-14 21:11:41 -0700

    Refactor: Rename App to BubblePhoneApp and implement basic UI
    This commit renames the main composable function from `App` to `BubblePhoneApp` for better clarity. It also implements a basic UI for the BubblePhone app, including:
    
    - A dark theme with specific color definitions.
    - A `Scaffold` layout.
    - A `LazyColumn` to display a list of app names (Messages, Photos, Browser, Notes, Settings) as `TextButton`s.
    - String resources for the app names.
    - Android Manifest updated to make the app a launcher and set `HOME` and `DEFAULT` categories.
    - iOS project configuration updated with a development team ID.

--- composeApp/src/iosMain/kotlin/com/jesusdmedinac/bubble/phone/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/jesusdmedinac/bubble/phone/MainViewController.kt
@@ -2,4 +2,4 @@ package com.jesusdmedinac.bubble.phone
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { BubblePhoneApp() }
